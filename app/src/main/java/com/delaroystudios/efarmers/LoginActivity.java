d24bc65b5e7f0d2ec9e30c8b488928d3ad5835fa
delaroystudios <RepubliC7891>
2020-05-19 16:27:52 +0100

    Login layout

---
+++ app/src/main/java/com/delaroystudios/efarmers/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.delaroystudios.efarmers;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import butterknife.ButterKnife;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
